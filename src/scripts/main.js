f405f809023c501ede15387f98cc726bc97fd057
aeschylus <scipioaffricanus@gmail.com>
2015-07-26 00:22:55 -0700

    iterative line layout complete. Lots of horrible layout code cruft lying around. Impossible to choose between the layouts. Probably broke the previous layout.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 var $ = require('jquery');
 var d3 = require('d3');
+var osd = require('./openseadragon');
 
 // D3 example
 $.get('http://purl.stanford.edu/fw090jw3474/iiif/manifest.json', function(data) {
@@ -9,10 +10,10 @@ $.get('http://purl.stanford.edu/fw090jw3474/iiif/manifest.json', function(data) 
 
 var manifestLayout = function(options) {
     var maxFrameHeight = options.maxFrameHeight || 130, // screen pixels
-        maxFrameWidth = options.maxFrameWidth ||  300, // screen pixels
+        maxFrameWidth = options.maxFrameWidth ||  30, // screen pixels
         minFrameWidth = options.minFrameWidth ||  30,  // screen pixels
         minFrameHeight = options.minFrameHeight ||  30,  // screen pixels
-        frameHeight = options.frameHeight ||  30,  // screen pixels
+        frameHeight = options.frameHeight ||  100,  // screen pixels
         frameWidth = options.frameWidth ||  30,  // screen pixels
         scaleFactor = options.scaleFactor || 1,
         columns = options.columns || 8,
@@ -106,18 +107,33 @@ var manifestLayout = function(options) {
             heightScaleFactor = frameHeight/canvas.height;
 
         if (portrait) {
-            canvas.height = frameHeight;
-            // we forced the height to fit, so the width
-            // must be scaled according to the height.
-            canvas.width = canvas.width*heightScaleFactor;
+            fitHeight(canvas, frameHeight);
         } else {
-            canvas.width = frameWidth;
-            // we forced the width to fit, so the height
-            // must be scaled according to the width.
-            canvas.height = canvas.height*widthScaleFactor;
+            fitWidth(canvas, frameWidth);
         }
 
-        console.log(canvas);
+        return canvas;
+    }
+    function fitHeight(canvas, frameHeight) {
+        var portrait = canvas.aspectRatio <= 1.0 ? true : false,
+            scaleFactor = frameHeight/canvas.height;
+
+        canvas.height = frameHeight;
+        // we forced the height to fit, so the width
+        // must be scaled according to the height.
+        canvas.width = canvas.width*scaleFactor;
+
+        return canvas;
+    }
+    function fitWidth(canvas, frameWidth) {
+        var portrait = canvas.aspectRatio <= 1.0 ? true : false,
+            scaleFactor = frameHeight/canvas.width;
+
+        canvas.width = frameWidth;
+        // we forced the width to fit, so the height
+        // must be scaled according to the width.
+        canvas.height = canvas.height*scaleFactor;
+
         return canvas;
     }
 
@@ -127,26 +143,17 @@ var manifestLayout = function(options) {
         // objects into a maximum width.
     }
 
-    function vantage(frames, padding) {
+    function vantage(frame, padding) {
         // A vantage can wrap several book pages
         // into what will become a single higlight,
         // hover, or click target.
-        console.log(padding);
-        if (frames[1]) {
-            return {
-                // width: (frames[0].width + frames[1].width)
-                //     + padding.left + padding.right + facingCanvasPadding,
-                // height: (frames[0].height + frames[1].height)
-                //     + padding.top + padding.bottom + facingCanvasPadding,
-                // frames: frames
-            };
-        } else {
-            return {
-                width: frameWidth + padding.left + padding.right,
-                height: frameHeight + padding.top + padding.bottom,
-                frames: frames
-            };
-        }
+        frame.localX = padding.left;
+        frame.localY = padding.top;
+        return {
+            width: frame.width + padding.left + padding.right,
+            height: frame.height + padding.top + padding.bottom,
+            frame: frame
+        };
     }
 
     function gridAlign(vantages, frameHeight, frameWidth, lineWidth) {
@@ -166,7 +173,7 @@ var manifestLayout = function(options) {
             vantage.x = vantage.width*lineIndex;
             vantage.y = lineNumber*vantage.height; // y determined by the line;
 
-            vantage.frames.forEach(function(frame) {
+            vantage.frames.forEach(function(frame, index) {
                 frame.localX = vantage.leftPadding,
                 frame.localY = vantage.topPadding,
                 frame.x = vantage.x + vantage.leftPadding,
@@ -199,43 +206,94 @@ var manifestLayout = function(options) {
         });
     }
 
+    // function layout() {
+    //     // Reads the configuration
+    //     // and structures the calculation with
+    //     // the appropriate strategies.
+
+    //     // Resolve the strategies used for the
+    //     // calculation based on the combination of
+    //     // options. These will all be functions
+    //     // to be run at the appropriate stage
+    //     // of the layout algorithm.
+    //     var frame = framingStrategies[framingStrategy];
+    //     var align = lineStrategies[lineStrategy];
+
+    //     // Prepare each node's layout parameters
+    //     // before passing them into the line-level
+    //     // functions.
+    //     var vantages = canvases.map(function(canvas) {
+    //         return vantage([frame(pruneCanvas(canvas),
+    //                               frameWidth,
+    //                               frameHeight
+    //                              )],
+    //                        vantagePadding,
+    //                        viewingHint
+    //                       );
+    //     });
+
+    //     // Take each vantage and mutate it to encompass
+    //     // facing pages, then use options to further
+    //     // mutate their x, y, width, and height coordinates
+    //     // to position them appropriately.
+    //     return align(bindPages(vantages, viewingHint, viewingDirection, facingCanvasPadding),
+    //                  frameHeight,
+    //                  frameWidth,
+    //                  containerWidth,
+    //                  viewingDirection
+    //                 );
+    // }
+    function fixedHeightAlign(vantages, lineWidth, viewingDirection) {
+        var lines = [];
+
+        lines.currentLine = 0;
+        lines.addLine = function() {
+            var line = lines[lines.currentLine] = [];
+            line.remaining = lineWidth;
+
+            return line;
+        };
+        lines.addItem = function(vantage) {
+            var line = this[this.currentLine];
+
+            if (!line) { line = this.addLine(); }
+
+            if (line.remaining >= vantage.width) {
+                var x = line.remaining;
+                line.remaining -= vantage.width;
+                return [lineWidth - x, lines.currentLine];
+            }
+
+            this.currentLine += 1;
+            line = lines.addLine();
+            x = line.remaining;
+            line.remaining -= vantage.width;
+            return [lineWidth - x, this.currentLine];
+        };
+
+        return vantages.map(function(vantage) {
+            var lineStats = lines.addItem(vantage);
+            vantage.x = lineStats[0];
+            vantage.y = lineStats[1]*vantage.height;
+            vantage.frame.x = vantage.x + vantage.frame.localX;
+            vantage.frame.y = vantage.y + vantage.frame.localY;
+            return vantage;
+        });
+    }
+
     function layout() {
-        // Reads the configuration
-        // and structures the calculation with
-        // the appropriate strategies.
-
-        // Resolve the strategies used for the
-        // calculation based on the combination of
-        // options. These will all be functions
-        // to be run at the appropriate stage
-        // of the layout algorithm.
-        var frame = framingStrategies[framingStrategy];
-        var align = lineStrategies[lineStrategy];
-
-        // Prepare each node's layout parameters
-        // before passing them into the line-level
-        // functions.
-        var vantages = canvases.map(function(canvas) {
-            return vantage([frame(pruneCanvas(canvas),
-                                  frameWidth,
-                                  frameHeight
-                                 )],
-                           vantagePadding,
-                           viewingHint
-                          );
+        var vantages = canvases.map(pruneCanvas).map(function(canvas) {
+            return vantage(fitHeight(canvas, frameHeight), vantagePadding);
         });
 
-        // Take each vantage and mutate it to encompass
-        // facing pages, then use options to further
-        // mutate their x, y, width, and height coordinates
-        // to position them appropriately.
-        return align(bindPages(vantages, viewingHint, viewingDirection, facingCanvasPadding),
-                     frameHeight,
-                     frameWidth,
-                     containerWidth,
-                     viewingDirection
-                    );
+        return fixedHeightAlign(vantages, containerWidth, viewingDirection);
     }
+
+    // Book
+    // Opening Focused
+    // Single Focused
+    // Top-to-bottom Focused
+    // Continuous Focused
 
     return layout();
 
@@ -252,40 +310,83 @@ function renderManifest(manifest) {
         frameWidth: 100,
         vantagePadding: {
             top: 0,
-            bottom: 50
+            bottom: 50,
+            left: 20,
+            right:20
         }
     });
 
-    // console.log(layoutData);
+    console.log(layoutData);
 
     var interactionOverlay = d3.select('#d3-example');
 
     // To understand this layout, read: http://bost.ocks.org/mike/nest/
     var vantages = interactionOverlay.selectAll('.vantage')
             .data(layoutData)
-            .enter().append('li')
+            .enter().append('div')
             .attr('class', 'vantage')
             .style('width', function(d) { return d.width + 'px'; })
             .style('height', function(d) { return d.height + 'px'; })
             .style('transform', function(d) { return 'translateX(' + d.x + 'px) translateY(' + d.y + 'px)'; });
 
-    // Now that the parents are bound, bind children.
-    // These are the official canvas placeholders, and
-    // will be filled with images on a promise.
-    vantages.selectAll('.frame')
-        .data(function(d) { return d.frames;})
-    // new enter selection specifically dealing with this data set.
-        .enter()
+    vantages
         .append('div')
         .attr('class', 'frame')
-        .style('width', function(d) { return d.width + 'px'; })
-        .style('height', function(d) { return d.height + 'px'; })
-        .style('transform', function(d) { return 'translateX(' + d.x + 'px) translateY(' + d.y + 'px)'; });
+        .style('width', function(d) { return d.frame.width + 'px'; })
+        .style('height', function(d) { return d.frame.height + 'px'; })
+        .style('transform', function(d) { return 'translateX(' + d.frame.localX + 'px) translateY(' + d.frame.localY + 'px)'; });
 
-    // Canvas labels, again the data is a nested subset of the vantages.
-    vantages.selectAll('h4')
-        .data(function(d) { return d.frames;})
-    // New enter selection specifically dealing with this data set.
-        .enter()
-        .append('h4').text(function(d) {return d.label;});
+    vantages
+        .append('h4').text(function(d) { return d.frame.label; });
 }
+
+// function renderManifest(manifest) {
+//     var container = $('#d3-example');
+
+//     var layoutData = manifestLayout({
+//         canvases: manifest.sequences[0].canvases,
+//         width: container.width(),
+//         height: container.height(),
+//         frameHeight: 100,
+//         frameWidth: 100,
+//         vantagePadding: {
+//             top: 0,
+//             bottom: 50,
+//             left: 20,
+//             right:20
+//         }
+//     });
+
+//     // console.log(layoutData);
+
+//     var interactionOverlay = d3.select('#d3-example');
+
+//     // To understand this layout, read: http://bost.ocks.org/mike/nest/
+//     var vantages = interactionOverlay.selectAll('.vantage')
+//             .data(layoutData)
+//             .enter().append('li')
+//             .attr('class', 'vantage')
+//             .style('width', function(d) { return d.width + 'px'; })
+//             .style('height', function(d) { return d.height + 'px'; })
+//             .style('transform', function(d) { return 'translateX(' + d.x + 'px) translateY(' + d.y + 'px)'; });
+
+//     // Now that the parents are bound, bind children.
+//     // These are the official canvas placeholders, and
+//     // will be filled with images on a promise.
+//     vantages.selectAll('.frame')
+//         .data(function(d) { return d.frames;})
+//     // new enter selection specifically dealing with this data set.
+//         .enter()
+//         .append('div')
+//         .attr('class', 'frame')
+//         .style('width', function(d) { return d.width + 'px'; })
+//         .style('height', function(d) { return d.height + 'px'; })
+//         .style('transform', function(d) { return 'translateX(' + d.x + 'px) translateY(' + d.y + 'px)'; });
+
+//     // Canvas labels, again the data is a nested subset of the vantages.
+//     vantages.selectAll('h4')
+//         .data(function(d) { return d.frames;})
+//     // New enter selection specifically dealing with this data set.
+//         .enter()
+//         .append('h4').text(function(d) {return d.label;});
+// }
