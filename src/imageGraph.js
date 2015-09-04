89755f4cfd8a82c453dbc49f0499d831763ab1ff
aeschylus <scipioaffricanus@gmail.com>
2015-09-04 10:44:51 -0700

    breaking it down

--- src/imageGraph.js
+++ src/imageGraph.js
@@ -1,93 +1,30 @@
-function renderLayout(layoutData, animate, callback) {
-  // To understand this render function,
-  // you need a general understanding of d3 selections,
-  // and you will want to read about nested
-  // selections in particular: http://bost.ocks.org/mike/nest/
+'use strict';
 
-  var interactionOverlay = d3.select(overlays[0]),
-      animationTiming = animate ? 1000 : 0;
+var manifestLayout = require('./manifestLayout');
+var canvasLayout = require('./canvasLayout');
+var iiif = require('./iiifUtils');
+var d3 = require('./lib/d3-slim-dist');
 
-  // var bounds = interactionOverlay.selectAll('.vantage')
-  //         .data(
-  //             (function() {
-  //                 return [layoutData.filter(function(frame){
-  //                     return frame.canvas.selected;
-  //                 })[0].vantage];
-  //             })())
-  //         .enter()
-  //         .append('div')
-  //         .attr('class', 'vantage')
-  //         .style('border', '3px solid orangered')
-  //         .style('box-sizing', 'border-box')
-  //         .style('width', function(d) { console.log (d); return d.width + 'px'; })
-  //         .style('height', function(d) { return d.height + 'px'; })
-  //         .style('position', 'absolute')
-  //         .transition()
-  //         .duration(animationTiming)
-  //         .ease('cubic-out')
-  //         .styleTween('transform', function(d) {
-  //             return d3.interpolateString(this.style.transform, 'translate(' + d.x +'px,' + d.y + 'px)');
-  //         })
-  //         .styleTween('-webkit-transform', function(d) {
-  //             return d3.interpolateString(this.style.transform, 'translate(' + d.x +'px,' + d.y + 'px)');
-  //         });
+function imageGraph(stores) {
 
-  var frame = interactionOverlay.selectAll('.' + frameClass)
-        .data(layoutData);
+  // creare a document fragment to draw "offline"
+  var graphRoot = document.createDocumentFragment();
 
-  var frameUpdated = frame
-        .style('width', function(d) { return d.width + 'px'; })
-        .style('height', function(d) { return d.height + 'px'; })
-        .transition()
-        .duration(animationTiming)
-        .ease('cubic-out')
-        .styleTween('transform', function(d) {
-          return d3.interpolateString(this.style.transform, 'translate(' + d.x +'px,' + d.y + 'px)');
-        })
-        .styleTween('-webkit-transform', function(d) {
-          return d3.interpolateString(this.style.transform, 'translate(' + d.x +'px,' + d.y + 'px)');
-        })
-        .tween('translateTilesources', translateTilesources)
-        .each(updateImages)
-        .call(endall, function() {
-          if (callback) { callback();}
-        });
+  var imageGraph = d3.select(sketchFrag).append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .call(renderImageScene);
 
-  frame.select('.' + canvasClass)
-    .style('width', function(d) { return d.canvas.width + 'px'; })
-    .style('height', function(d) { return d.canvas.height + 'px'; })
-    .attr('class', function(d) {
-      var selected = d.canvas.selected;
-      return selected ? canvasClass + ' selected' : canvasClass;
-    });
+  function renderImageScene(selection) {
+  }
 
-  var frameEnter = frame
-        .enter().append('div')
-        .attr('class', frameClass)
-        .style('width', function(d) { return d.width + 'px'; })
-        .style('height', function(d) { return d.height + 'px'; })
-        .style('transform', function(d) { return 'translate(' + d.x + 'px,' + d.y + 'px)'; })
-        .style('-webkit-transform', function(d) { return 'translate(' + d.x + 'px,' + d.y + 'px)'; });
+  // This renders the graph of nodes according to node law.
+  // The land of the nodes is a cruel and harsh one,
+  // but for the nodes, who have inhabited these
+  // barren wastes for eons now,
+  // it will always be home.
 
-  frameEnter
-    .append('div')
-    .attr('class', function(d) {
-      var selected = d.canvas.selected;
-      return selected ? canvasClass + ' selected' : canvasClass;
-    })
-    .attr('data-id', function(d) {
-      return d.canvas.id;
-    })
-    .style('width', function(d) { return d.canvas.width + 'px'; })
-    .style('height', function(d) { return d.canvas.height + 'px'; })
-    .style('transform', function(d) { return 'translateX(' + d.canvas.localX + 'px) translateY(' + d.canvas.localY + 'px)'; })
-    .each(enterImages);
-  // .append('img')
-  // .attr('src', function(d) { return d.canvas.iiifService + '/full/' + Math.ceil(d.canvas.width * 2) + ',/0/default.jpg';});
-
-  frameEnter
-    .append('div')
-    .attr('class', labelClass)
-    .text(function(d) { return d.canvas.label; });
-
+  return imageGraph;
 };
+
+module.exports = imageGraph;
--- src/main.js
+++ src/main.js
@@ -1,174 +1,23 @@
 'use strict';
 
-var manifestLayout = require('./manifestLayout');
-var canvasLayout = require('./canvasLayout');
-var iiif = require('./iiifUtils');
+var viewStateStore = require('./viewStateStore'),
+    imageGraph = require('./imageGraph'),
+    domComponent = require('./domComponent');
 
 var manifestor = function(options) {
-  var manifest = options.manifest,
-      sequence = options.sequence,
-      canvases = options.sequence ? options.sequence.canvases : manifest.sequences[0].canvases,
-      container = options.container,
-      initialViewingDirection = options.viewingDirection ? options.viewingDirection : getViewingDirection(),
-      initialViewingMode = options.viewingMode ? options.viewingHint : getViewingHint(),
-      initialPerspective = options.perspective ? options.perspective : 'overview',
-      selectedCanvas = options.selectedCanvas,
-      viewer,
-      canvasClass = options.canvasClass ? options.canvasClass : 'canvas',
-      frameClass = options.frameClass ? options.frameClass : 'frame',
-      labelClass = options.labelClass ? options.labelClass : 'label',
-      stateUpdateCallback = options.stateUpdateCallback,
-      _canvasState,
-      _canvasImageStates,
-      _zooming = false,
-      _constraintBounds = {x:0, y:0, width:container.width(), height:container.height()},
-      _inZoomConstraints;
+  var manifestor = viewStateStore(options);
 
-  buildCanvasStates(canvases);
+  manifestor.dom = domComponent(options.container);
+  manifestor.osd = manifestor.canvas.osd;
 
-  // set the initial state, which triggers the first rendering.
-  canvasState({
-    selectedCanvas: selectedCanvas, // @id of the canvas:
-    perspective: initialPerspective, // can be 'overview' or 'detail'
-    viewingMode: initialViewingMode, // manifest derived or user specified (iiif viewingHint)
-    viewingDirection: initialViewingDirection, // manifest derived or user specified (iiif viewingHint)
-    width: container.width(),
-    height: container.height()
-  }, true); // "initial" is true here; don't fire the state callback.
+  manifestor.osd = OpenSeadragon({
+    element: manifestor.dom.osdElement,
+    showNavigationControl: false
+  });
 
-  function canvasState(state, initial) {
+  manifestor.graphRoot = {};
 
-    if (!arguments.length) return _canvasState;
-    _canvasState = state;
-
-    if (stateUpdateCallback && !initial) {
-      // should we pass in the state here?
-      // I don't really want to encourage reading
-      // the state from the event.
-      stateUpdateCallback();
-    }
-
-    return _canvasState;
-  }
-
-  function canvasImageStates(state) {
-
-    if (!arguments.length) return _canvasImageStates;
-    _canvasImageStates = state;
-
-    // if (!initial) {
-    //     jQuery.publish('annotationsTabStateUpdated' + this.windowId, this.tabState);
-    // }
-
-    return _canvasImageStates;
-  }
-
-  function selectCanvas(item) {
-    var state = canvasState();
-    state.selectedCanvas = item;
-    state.perspective = 'detail';
-    canvasState(state);
-  }
-
-  function selectPerspective(perspective) {
-    var state = canvasState();
-    state.previousPerspective = state.perspective;
-    state.perspective = perspective;
-    canvasState(state);
-  }
-
-  function selectViewingMode(viewingMode) {
-    var state = canvasState();
-    state.viewingMode = viewingMode;
-
-    canvasState(state);
-  }
-
-  function refreshState(newState) {
-    var state = canvasState();
-
-    // for blah in blah overwrite blah
-    // rather than just setting a specific
-    // property.
-    canvasState(state);
-  }
-
-  function addImageCluster(id) {
-    var canvases = canvasImageStates();
-
-    canvases[id] = {
-    };
-  }
-
-  function addDummyObj(id, osdTileObj) {
-    var canvasStates = canvasImageStates();
-
-    canvasStates[id].dummyObj = osdTileObj;
-
-    canvasImageStates(canvasStates);
-  }
-
-  function buildCanvasStates(canvases) {
-    var canvasStates = {};
-
-    canvases.forEach(function(canvas) {
-      canvasStates[canvas['@id']] = {
-        tileSourceUrl: canvas.images[0].resource.service['@id'] + '/info.json'
-      };
-    });
-
-    canvasImageStates(canvasStates);
-  }
-
-  function resize() {
-    var state = canvasState();
-
-    state.width = container.width();
-    state.height = container.height();
-
-    canvasState(state);
-  }
-
-  function updateThumbSize(scaleFactor) {
-    var state = canvasState();
-
-    state.scaleFactor = scaleFactor;
-
-    canvasState(state);
-  }
-
-  function updateConstraintBounds(bounds) {
-    var state = canvasState();
-
-    // This should probably be integrated into
-    // some other type of store, such as
-    // one that handles state that is
-    // updated in real time (zoom level,
-    // current bounds, "_zooming", and
-    // this).
-
-    // state.constraintBounds = bounds;
-    _constraintBounds = bounds;
-
-    // canvasState(state);
-  }
-
-  return {
-    // selectMode: selectMode,
-    // selectPerspective: selectPerspective,
-    // next: next,
-    // previous: previous,
-    // scrollThumbs: scrollThumbs,
-    resize: resize,
-    selectCanvas: selectCanvas,
-    selectPerspective: selectPerspective,
-    selectViewingMode: selectViewingMode,
-    updateThumbSize: updateThumbSize,
-    refreshState: refreshState,
-    getState: canvasState,
-    setState: canvasState,
-    osd: viewer
-  };
+  return manifestor;
 };
 
 module.exports = manifestor;
