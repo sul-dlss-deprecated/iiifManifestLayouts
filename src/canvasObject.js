c0975694f88e43c4974bb2acc256a8a55dcf94e4
nein09 <nein09@nein09.com>
2015-11-13 16:10:32 -0800

    Fix tabs

--- src/canvasObject.js
+++ src/canvasObject.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var canvasObject= function(canvas) {
-	return {
-		needed: false,
-		visible: true,
-		clipRegion: null,
-		opacity: 1,
-		position: {
-			x: 0,
-			y: 0,
-		},
-		placeholder: '/some-image.jpg',
+    return {
+        needed: false,
+        visible: true,
+        clipRegion: null,
+        opacity: 1,
+        position: {
+            x: 0,
+            y: 0,
+        },
+        placeholder: '/some-image.jpg',
         tileSourceUrl: canvas.images[0].resource.service['@id'] + '/info.json',
-      }
+    }
 };
 
 module.exports = canvasObject;
