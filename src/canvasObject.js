5bb1df0085f83d6a418ba00659ecb41b59e2e627
nein09 <nein09@nein09.com>
2015-11-16 09:53:00 -0800

    Don't clobber existing attributes in the canvas object

--- src/canvasObject.js
+++ src/canvasObject.js
@@ -1,18 +1,21 @@
 'use strict';
 
+var _ = require('underscore');
+
+var defaults = {
+    needed: false,
+    visible: true,
+    clipRegion: null,
+    opacity: 1,
+    position: {
+        x: 0,
+        y: 0,
+    },
+    placeholder: '/some-image.jpg',
+};
 var canvasObject= function(canvas) {
-    return {
-        needed: false,
-        visible: true,
-        clipRegion: null,
-        opacity: 1,
-        position: {
-            x: 0,
-            y: 0,
-        },
-        placeholder: '/some-image.jpg',
-        tileSourceUrl: canvas.images[0].resource.service['@id'] + '/info.json',
-    }
+    canvas.tileSourceUrl = canvas.images[0].resource.service['@id'] + '/info.json';
+    return _.defaults(canvas, defaults);
 };
 
 module.exports = canvasObject;
