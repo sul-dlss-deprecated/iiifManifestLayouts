4fb2517e687353b280ecf715aa143619c9f7e18c
nein09 <nein09@nein09.com>
2015-11-16 11:07:37 -0800

    Fix tabs

--- src/canvasObject.js
+++ src/canvasObject.js
@@ -3,19 +3,19 @@
 var _ = require('underscore');
 
 var defaults = {
-    needed: false,
-    visible: true,
-    clipRegion: null,
-    opacity: 1,
-    position: {
-        x: 0,
-        y: 0,
-    },
-    placeholder: '/some-image.jpg',
+  needed: false,
+  visible: true,
+  clipRegion: null,
+  opacity: 1,
+  position: {
+      x: 0,
+      y: 0,
+  },
+  placeholder: '/some-image.jpg',
 };
 var canvasObject= function(canvas) {
-    canvas.tileSourceUrl = canvas.images[0].resource.service['@id'] + '/info.json';
-    return _.defaults(canvas, defaults);
+  canvas.tileSourceUrl = canvas.images[0].resource.service['@id'] + '/info.json';
+  return _.defaults(canvas, defaults);
 };
 
 module.exports = canvasObject;
