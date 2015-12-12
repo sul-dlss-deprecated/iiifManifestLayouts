38ad6af0b9a1ad2f8ad64e8651e232dc55466827
nein09 <nein09@nein09.com>
2015-12-11 17:15:59 -0800

    WIP on choice and specific configs

--- src/imageResourceFactory.js
+++ src/imageResourceFactory.js
@@ -3,6 +3,16 @@
 require('openseadragon');
 var ImageResource = require('./ImageResource');
 
+var _getSegmentFromUrl = function(url) {
+  var urlParts = url.split('#');
+  var segment = null;
+  if(urlParts.length > 1) { // the url has a segment specified
+    var rectArray = urlParts.split('=').split(',');
+    segment = new OpenSeadragon.Rect(rectArray[0], rectArray[1], rectArray[2], rectArray[3]);
+  }
+  return segment;
+};
+
 var _buildImageConfig = function(resource) {
   var hasService = !!(resource.service);
   var idObj = resource;
@@ -22,66 +32,68 @@ var _buildImageConfig = function(resource) {
     }
   };
 
-  var _getSegmentFromUrl = function(url) {
-    var urlParts = url.split('#');
-    var segment = null;
-    if(urlParts.length > 1) { // the url has a segment specified
-      var rectArray = urlParts.split('=').split(',');
-      segment = new OpenSeadragon.Rect(rectArray[0], rectArray[1], rectArray[2], rectArray[3]);
-    }
-    return segment;
-  };
-
   var imageTileSource =  _getImageTilesource();
 
   return {
     tileSource: imageTileSource,
     clipRegion: _getSegmentFromUrl(id),
-    bounds: _getSegmentFromUrl(image.on),
     dynamic: hasService // todo: it's more complicated than that
   };
 };
 
 
-var _buildChoiceConfigs = function(choice) {
+var _buildChoiceConfigs = function(resource) {
+
+  var _buildImageChoice = function(item, type, zIndex) {
+    var config = _buildImageConfig(item);
+    config.imageType = type;
+    config.label = item.label;
+    config.zIndex = zIndex;
+  }
   var configs = [];
-  var defaultConfig = _buildImageConfig(choice.default);
-  defaultConfig.imageType = 'default';
-  configs.push(defaultConfig);
+  configs.push(_buildImageChoice(resource.default, 'main', 0));
+
+  resource.item.forEach(function(item) {
+    configs.push(_buildImageChoice(item), 'alternate', 1);
+  });
   return configs;
-};
-
-var _buildSpecificConfig = function(specificResource) {
-
 };
 
 var ImageResourceFactory = function(image, parent) {
   var resourceType = image.resource['@type']; // can be oa:Choice, oa:SpecificResource, or dctypes:Image
 
+  var _addConfigAttributes = function(config) {
+    config.parent = parent;
+    config.bounds = _getSegmentFromUrl(image.on);
+  };
+
   switch(resourceType) {
     case 'dctypes:Image':
       var config = _buildImageConfig(image.resource);
-      config.parent = parent;
-      return new ImageResource(config); // maybe add this directly to the parent instead
+      _addConfigAttributes(config);
+      return new ImageResource(config);
       break;
     case 'oa:Choice':
-      console.log('oa:Choice!', image);
-      var configs = _buildChoiceConfigs(image);
-      configs.forEach(function(config) {
-        // todo: return something sensible
-        // add each of these images to the parent
-      })
+      var configs = _buildChoiceConfigs(image.resource);
+      return configs.map(function(config) {
+        _addConfigAttributes(config);
+        return new ImageResource(config);
+      });
       break;
     case 'oa:SpecificResource':
-      console.log('specific resource!', image);
-      config = _buildSpecificConfig(image);
+      var config = _buildImageConfig(image.resource);
+      _addConfigAttributes(config);
+      if(image.selector && image.selector.region) {
+        var clipArray = image.selector.region.split(',');
+        var clipRect = new OpenSeadragon.Rect(clipArray[0], clipArray[1], clipArray[2], clipArray[3]);
+        config.clipRegion = clipRect;
+        config.zIndex = 0;
+      }
+      return new ImageResource(config);
       break;
     default:
       throw new Error("Cannot create an image from type " + resourceType);
   }
-
-  config.parent = parent;
-  return new ImageResource(config);
 };
 
 module.exports = ImageResourceFactory;
