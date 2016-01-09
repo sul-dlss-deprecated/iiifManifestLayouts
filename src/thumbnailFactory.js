794060c431de07a85e138a7bfce66957e63803d4
nein09 <nein09@nein09.com>
2016-01-08 17:44:46 -0800

    Force static thumbnails and legacy tilesource

--- src/thumbnailFactory.js
+++ src/thumbnailFactory.js
@@ -3,31 +3,31 @@
 var ImageResource = require('./ImageResource');
 
 var _getThumbUrl = function(resource, width) {
-  if(resource.service) {
-    return resource.service['@id'] + '/full/' + Math.ceil(width) + ',/0/default.jpg';
-  } else if(resource.default) {
-    return _getThumbUrl(resource.default, width);
+  if(resource.default) {
+    return resource.default.id;
   } else {
     return resource['@id'];
   }
 };
 
-var _getThumbLevel = function(canvas, divisor) {
+var _getThumbLevel = function(resource) {
+  if(resource.default) {
+    return _getThumbLevel(resource.default);
+  }
+
   return {
-    url: _getThumbUrl(canvas.images[0].resource, canvas.width / divisor),
-    height: canvas.height / divisor,
-    width: canvas.width / divisor,
+    url: resource['@id'],
+    height: resource.height,
+    width: resource.width,
   }
 };
 
-var _makeThumbnailConfig = function(canvas, parent) {
+var _makeThumbnailConfig = function(resource, parent) {
   return {
     tileSource: {
       type: 'legacy-image-pyramid',
       levels: [
-        _getThumbLevel(canvas, 4),
-        _getThumbLevel(canvas, 8),
-        _getThumbLevel(canvas, 16)
+        _getThumbLevel(resource),
       ]
     },
     parent: parent,
@@ -44,15 +44,9 @@ var ThumbnailFactory = function(canvas, parent) {
 
   // If the canvas has no thumbnail object, we try to fall back to using an image from it.
   // If the canvas has no images and no thumbnail, we can't do anything, so we don't bother.
-  // If there is no thumbnail and only one image in this canvas, there's no reason to make a thumbnail for it-
-  // the canvasobject will fall back to opening the main tilesource in the absence of a thumbnail.
-  if(canvas.images && canvas.images.length > 1) {
-    var config = _makeThumbnailConfig(canvas, parent);
+  if(canvas.images) {
+    var config = _makeThumbnailConfig(canvas.images[0].resource, parent);
     return new ImageResource(config);
-  }
-
-  if(canvas.images.length == 1) {
-    console.log("Only one image");
   }
 };
 
