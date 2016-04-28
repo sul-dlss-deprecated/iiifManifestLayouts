72fbdd5a81963fb1e11d0e7332eecbb59ca711e5
nein09 <nein09@nein09.com>
2016-04-27 17:52:15 -0700

    Give viewerState its own class

---
+++ src/viewerState.js
@@ -0,0 +1,29 @@
+var viewerState = function(config) {
+  this.updateCallback = config.updateCallback;
+
+  this.state = {
+    selectedCanvas: config.selectedCanvas, // @id of the canvas:
+    perspective: config.perspective, // can be 'overview' or 'detail'
+    viewingMode: config.viewingMode, // manifest derived or user specified (iiif viewingHint)
+    viewingDirection: config.viewingDirection, // manifest derived or user specified (iiif viewingHint)
+    width: config.width,
+    height: config.height
+  };
+};
+
+viewerState.prototype = {
+  getState: function() {
+    return this.state;
+  },
+
+  setState: function(newState) {
+    for(var key in newState) {
+      this.state[key] = newState[key];
+    }
+    if(this.updateCallback) {
+      this.updateCallback();
+    }
+  }
+};
+
+module.exports = viewerState;
