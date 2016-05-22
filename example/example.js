0e8111ea06864057ae4dcdfd4048c0c3ade054ab
Tom Crane <tom@nanoplanet.com>
2016-05-22 22:49:42 +0100

    Support thumbnails with no services (with Wellcome example)

--- example/example.js
+++ example/example.js
@@ -15,7 +15,8 @@ var App = {
    {url: 'http://demos.biblissima-condorcet.fr/iiif/metadata/BVMM/chateauroux/manifest.json', label: 'BNF Detail Images Demo (Chateauroux)'},
    {url:'http://manifests.ydc2.yale.edu/manifest/Osbornfa1v2.json', label: "Yale Osborn with choice (see 53r)"},
    {url: 'http://dms-data.stanford.edu/data/manifests/BnF/jr903ng8662/manifest.json', label: 'Stanford DMS Manuscript (example of typical object)'},
-   {url: 'http://iiif.ub.uni-leipzig.de/0000000001/manifest.json', label: 'Leipzig Scroll'}
+   {url: 'http://iiif.ub.uni-leipzig.de/0000000001/manifest.json', label: 'Leipzig Scroll'},
+   {url: 'http://tomcrane.github.io/scratch/manifests/foldout-as-choice.json', label: 'Pseudo-Albert the Great (second canvas)'}
    ].forEach(function(fixture) {
     $('<option>')
       .val(fixture.url)
